4bbf06f6b4a63f2b6da5d468f16ccb1035632107
Andrey Park <ap.techculture@gmail.com>
2021-10-15 00:17:41 +0600

    feat: complete all main tasks

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useMemo } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector, RootStateOrAny } from 'react-redux';
-import { useHistory } from 'react-router';
+import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
+
 import { Modal } from '@mui/material';
 
 //thunks
@@ -10,69 +9,79 @@ import { asyncGetUsersThunk } from '../../store/Users/UsersThunk';
 import { Header } from '../../components/Header';
 import { Loader } from '../../components/Loader';
 import { Table } from '../../components/Table';
+
 export const MainPage = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(
     (state: RootStateOrAny) => state.users.isLoading
   );
-  const history = useHistory();
 
   const [users, setUsers] = useState([
     {
+      checked: true,
       id: 1,
       username: 'Bret',
       email: 'Sincere@april.biz',
       website: 'hildegard.org',
     },
     {
+      checked: false,
       id: 2,
       username: 'Antonette',
       email: 'Shanna@melissa.tv',
       website: 'anastasia.net',
     },
     {
+      checked: false,
       id: 3,
       username: 'Samantha',
       email: 'Nathan@yesenia.net',
       website: 'ramiro.info',
     },
     {
+      checked: false,
       id: 4,
       username: 'Karianne',
       email: 'Julianne.OConner@kory.org',
       website: 'kale.biz',
     },
     {
+      checked: false,
       id: 5,
       username: 'Kamren',
       email: 'Lucio_Hettinger@annie.ca',
       website: 'demarco.info',
     },
     {
+      checked: false,
       id: 6,
       username: 'Leopoldo_Corkery',
       email: 'Karley_Dach@jasper.info',
       website: 'ola.org',
     },
     {
+      checked: false,
       id: 7,
       username: 'Elwyn.Skiles',
       email: 'Telly.Hoeger@billy.biz',
       website: 'elvis.io',
     },
     {
+      checked: false,
       id: 8,
       username: 'Maxime_Nienow',
       email: 'Sherwood@rosamond.me',
       website: 'jacynthe.com',
     },
     {
+      checked: false,
       id: 9,
       username: 'Delphine',
       email: 'Chaim_McDermott@dana.io',
       website: 'conrad.com',
     },
     {
+      checked: false,
       id: 10,
       username: 'Moriah.Stanton',
       email: 'Rey.Padberg@karina.biz',
@@ -82,8 +91,35 @@ export const MainPage = () => {
 
   const columns = useMemo(
     () => [
-      { Header: '', accessor: 'checkbox' },
-      { Header: 'ID', accessor: 'id' },
+      {
+        Header: 'Checkbox',
+        id: 'checkbox',
+        accessor: (str: any) => 'checkbox',
+        Cell: (row: any) => {
+          return (
+            <input
+              type='checkbox'
+              checked={row.row.original.checked}
+              onChange={() => {
+                let total = row.data.filter((item: any) => item.checked);
+                if (total.length > 1 || !row.data[row.row.id].checked) {
+                  const item = row.data[row.row.id];
+                  const newItem = { ...item, checked: !item.checked };
+                  setUsers([
+                    ...row.data.slice(0, row.row.id),
+                    newItem,
+                    ...row.data.slice(+row.row.id + 1),
+                  ]);
+                }
+              }}
+            />
+          );
+        },
+      },
+      {
+        Header: 'ID',
+        accessor: 'id',
+      },
       {
         Header: 'More information',
         accessor: 'more',
